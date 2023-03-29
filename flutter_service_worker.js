9b4d3d22058b7c3e6b208f1b2b3bbd73a7ae8406
Fahad Turki Alazmi <41303127+fahad0100@users.noreply.github.com>
2023-03-29 07:16:34 +0300

    fix pdf
    fix pdf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "dddc7b5f83427bede632899dc2a56ded",
-"index.html": "73fb104a52677253478b161615698a54",
-"/": "73fb104a52677253478b161615698a54",
+"index.html": "7dde8dae620f3d5e62237187217c9fa0",
+"/": "7dde8dae620f3d5e62237187217c9fa0",
 "main.dart.js": "2e8755d03b7e60faffc8ca723cf172b5",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -26,11 +25,7 @@ const RESOURCES = {
 "assets/assets/icons/linkdin.png": "4dd04996956aab18c8af7051b65ab4bb",
 "assets/assets/icons/twitter.png": "1d35c2dcbe0932bd3800ff06a860460f",
 "assets/assets/icons/phone-call.png": "9e299bb392428812cffbdadaf9272b15",
-"assets/assets/icons/cv.pdf": "5eeb6e77ccc35da1a52f3170d2641214",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/icons/cv.pdf": "5eeb6e77ccc35da1a52f3170d2641214"
 };
 
 // The application shell files that are downloaded before a service worker can
